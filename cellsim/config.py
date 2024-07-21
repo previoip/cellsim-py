e12359dc08745452696cecf99443f46455208825
Previo Prakasa <previo.prakasa@gmail.com>
2024-07-21 19:29:27 +0700

    update config.ini dataset enum

--- cellsim/config.py
+++ cellsim/config.py
@@ -2,6 +2,53 @@ import os
 import configparser
 from dataclasses import dataclass, asdict, fields
 
+@dataclass
+class ConfigSectionDataset:
+  download_dir: str = './downloads'
+  extract_dir: str = './data'
+  variants: str = 'movielens'
+
+  @property
+  def _section_name(self):
+    return 'dataset'
+
+  @property
+  def variants_list(self):
+    return self.variants.splitlines()
+
+
+@dataclass
+class ConfigSectionDatasetMovielens:
+  name: str = 'movielens'
+  host: str = 'http://files.grouplens.org'
+  path: str = 'datasets/movielens'
+  variants: str = 'ml-100k\nml-1m\nml-10m\nml-20m\nml-25m\nml-32m'
+
+  @property
+  def _section_name(self):
+    return 'dataset.movielens'
+
+  @property
+  def variants_list(self):
+    return self.variants.splitlines()
+
+@dataclass
+class ConfigSectionDatasetMovielensVariant:
+  archive_name: str = 'ml-100k.zip'
+  archive_hash: str = 'MD5 (ml-100k.zip) = 0e33842e24a9c977be4e0107933c0723'
+  data_file: str = 'u.data'
+  delimiter: str = '\t'
+  colname_uid: str = 'userId'
+  colname_iid: str = 'movieId'
+  colname_ts: str = 'timestamp'
+  colname_inter: str = 'rating'
+  colsequence: str = 'uid|iid|inter|ts'
+  use_colnames_as_headers: str = 'true'
+
+  @property
+  def _section_name(self):
+    return 'dataset.movielens.' + self.archive_name.split('.')[0]
+
 
 @dataclass
 class ConfigSectionSimDefault:
@@ -75,6 +122,9 @@ class Config:
 
   def __init__(self, file_path):
     self._file_path = file_path
+    self.dataset = ConfigSectionDataset()
+    self.dataset_info = ConfigSectionDatasetMovielens()
+    self.dataset_variants = dict()
     self.default: ConfigSectionSimDefault = ConfigSectionSimDefault()
     self.cache: ConfigSectionSimCache = ConfigSectionSimCache()
     self.recsys: ConfigSectionSimRecsys = ConfigSectionSimRecsys()
@@ -87,19 +137,37 @@ class Config:
       self.recsys,
       self.env,
       self.rl,
+      self.dataset,
+      self.dataset_info,
     ]
 
   def save(self):
     for namespace in self._namespaces:
       self._parser[namespace._section_name] = asdict(namespace)
+
+    for dataset_name, dataset_namespace in self.dataset_variants.items():
+      dataset_section = self.dataset_info._section_name + '.' + dataset_name
+      self._parser[dataset_section] = asdict(dataset_namespace)
+
     with open(self._file_path, 'w') as fp:
       self._parser.write(fp)
 
   def load(self):
     if not os.path.exists(self._file_path):
       self.save()
+
     with open(self._file_path, 'r') as fp:
       self._parser.read_file(fp)
+
     for namespace in self._namespaces:
       for field in fields(namespace):
         setattr(namespace, field.name, field.type(self._parser[namespace._section_name][field.name]))
+
+    for dataset_name in self.dataset_info.variants_list:
+      dataset_section = self.dataset_info._section_name + '.' + dataset_name
+      namespace = ConfigSectionDatasetMovielensVariant()
+      if not dataset_section in self._parser:
+        continue
+      for field in fields(namespace):
+        setattr(namespace, field.name, field.type(self._parser[dataset_section][field.name]))
+      self.dataset_variants[dataset_name] = namespace
